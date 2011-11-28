e844ba046485321b9e985296fbd7a90ab24acc1d
Francis Addai <4addai@gmail.com>
2011-11-28 02:42:32 +0000

    Corrected method definitions in the Person object

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var Person =  function(opt){
     this.age = opt.age || 0;
     this.eyeColor = opt.eyeColor || "Blue";
     
-    this.getName: function(){
+    this.getName = function(){
         return this.name;
-    },
+    };
 
-    this.getBio: function(){
+    this.getBio = function(){
         return "My name is "+ this.getName() + " and I am a "+ this.age + " year old\nwith "+ this.eyeColor +" eye color.";
-    }
+    };
  
 }
 
